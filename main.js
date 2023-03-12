e75800b6308cfc91eacdba6a034831dfcc718ee1
JVE12 <99225048+JVE12@users.noreply.github.com>
2023-03-12 16:11:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+song = "";
+lwx = 0;
+lwy = 0;
+rwx = 0;
+rwy = 0;
+
+//function preload() {
+ //   jap = loadSound("jap.mp3");
+ //   harry = loadSound("harry.mp3");
+ //   jap.setVolume(1);
+ //   jap.rate(1);
+ //   harry.setVolume(1);
+   // harry.rate(1);
+//}
+function setup() {
+
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+ //   canvas = createCanvas(600, 500);
+    //canvas.center();
+
+
+  //  poseNet = ml5.poseNet(video, modelLoaded);
+  //  poseNet.on('pose', gotPoses);
+}
+
+//function gotPoses(results) {
+  //  if(results.lenght > 0) {
+   //     console.log(results);
+    //    lwx = results[0].pose.leftWrist.x;
+    //    lwy = results[0].pose.lefttWrist.y;
+
+    //    rwx = results[0].pose.rightWrist.x;
+    //    rwy = results[0].pose.rightWrist.y;
+   // }
+//}
+//function modelLoaded()
+//{
+  //  console.log("PoseNet Is Initialized");
+
+//}
+
+//function draw() {
+//}
+
+//function play() {
+  //  jap.play();
+//}
